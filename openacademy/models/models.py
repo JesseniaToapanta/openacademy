12b308dda182268230fb58e9911a59a09fa8bd57
Jessenia Toapanta <jesseniatpt@gmail.com>
2019-11-12 12:32:30 -0500

    [REF constraints]

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, exceptions
 import time 
+from psycopg2 import IntegrityError
    
 def get_uid(self, *a):
 #si se desea ver que nomas tiene la variable usar pdb
@@ -22,6 +23,50 @@ class Course(models.Model):
      #default=lambda self, *a: self.env.uid)
      default=get_uid)
      session_ids = fields.One2many('openacademy.session', 'course_id')
+
+#tambén se pueden usar los contrains de postgresql
+#para ello es necesario declarar l variable reservada _sql_contrains
+     _sql_constraints = [
+         ('name_description_check',
+          'CHECK( name != description)',
+          "The title of the course should not be the same description"),
+         ('name_unique',
+          'UNIQUE(name)',
+          "The course title must be unique",
+         ),             
+     ]
+
+#ESTA MANERA SÓLO FUNCIONA VEZ
+#PORQUE ESTÁ CONCATENADO ' otro' por tanto la segunda vez será un registro duplicado
+     def copy(self, default=None):
+         if default is None:
+             default = {} 
+#       entonces utiliza un dicionario vació
+#PARA SOLUCIONAR LA DUCLICIDAD AL COPIAR aumentar copied_count
+         copied_count = self.search_count([('name', 'ilike', 'Copy of %s%%' %(self.name))])
+         if not copied_count:
+            new_name = "Copy of %s" % (self.name)
+         else:
+            new_name = "Copy of %s (%s)"% (self.name, copied_count)
+#primero contamos cuantos existen con ese nombre
+# % en Python es para concatenar
+# % en posgres es cualquier cosa similar a (lo que esté antes)
+#         default['name'] = self.name + ' otro'    # se reemplaza por
+         default['name'] = new_name
+# para visualizar la excepcion que me devuelve a l duplicar un dato se usa pdb
+#         import pdb; pdb.set_trace()
+#         try:
+         return super(Course, self).copy(default)
+#         except IntegrityError:
+#             import pdb; pdb.set_trace()
+            # print e
+#       return super(NombreClasePython, self).copy(nombreParámetro)
+#       pero cómo se q puedo acceder al método .copy
+#       por que model hereda de la clase Model que está en el repositorio local de odoo
+#      (models.Model)
+#       si redefino el método original, con el super mando a llamar el método 
+#       pero cambiando el compartamiento particular, en este caso es
+#       concatenando ' otro'
 
 class Session(models.Model):
      _name = 'openacademy.session'
@@ -45,16 +90,46 @@ class Session(models.Model):
      string="Course", required=True)
      
      attendee_ids = fields.Many2many('res.partner', string="Attendees")
-     taken_seats = fields.Float(compute='_taken_seats')
+#   taken_seats es un campo calculado que no existe en la base de datos 
+#   para  que se agregue a la base de datos se le aumenta el parámetro store=True
+     taken_seats = fields.Float(compute='_taken_seats', store=True)
 #   campo reservado active, siempre debe ser decclarado como boolean
 #   por defecto será true, es decir que al crear una sesión estará activa
      active = fields.Boolean(default=True)
 
      @api.depends('seats','attendee_ids')
      def _taken_seats(self):
-#        import pdb;pdb.set_trace()
+#         import pdb;pdb.set_trace()
 #        for record in self:
 #            if not record.seats: record.taken_seats = 0.0  
 #            else: record.taken_seats = 100.0 * len(record.attendee_ids) / record.seats
          for record in self.filtered(lambda r: r.seats):
              record.taken_seats = 100.0 * len(record.attendee_ids) / record.seats
+
+     @api.onchange('seats', 'attendee_ids')
+     def _verify_valid_seats(self):
+#         if self.seats < 0:
+         if self.filtered(lambda r: r.seats < 0):
+             self.active = False
+             return {
+                'warning': {
+                    'title': "Incorrect 'seats' value",
+                    'message': "The number of available seats may not be negative",      
+                            }
+                    }
+         if self.seats < len(self.attendee_ids):
+             self.active = False
+             return{
+                 'warning':{
+                     'title': "Too many attendees",
+                     'message': "Increase seats or remove excess attendess",
+                           }
+                    }
+         self.active = True
+
+     @api.constrains('instructor_id', 'attendee_ids')
+     def _check_instructor_not_in_attendees(self):
+          for record in self.filtered('instructor_id'):
+              if record.instructor_id in record.attendee_ids:
+                  raise exceptions.ValidationError(
+				"A session's instructor can't be an attendee")
